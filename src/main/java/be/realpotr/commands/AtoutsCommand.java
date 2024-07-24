cc7d3898efa177b13fef7d9c354124fb97cc756d
realpotr <mavci1228@gmail.com>
2024-07-24 21:01:34 +0200

    v1.0

--- src/main/java/be/realpotr/commands/AtoutsCommand.java
+++ src/main/java/be/realpotr/commands/AtoutsCommand.java
@@ -3,11 +3,20 @@ package be.realpotr.commands;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class AtoutsCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+            sender.sendMessage("Ouverture des atouts");
+        } else {
+            sender.sendMessage("Cette commande ne peut être exécutée que par un joueur.");
+        }
+
+
 
         return true;
     }
